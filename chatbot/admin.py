a84125e3583f20d33d95d0d46ba3e51d87048d2c
J.Alberto Azevedo <alazib.developer@gmail.com>
2025-06-11 15:05:58 +0200

    create admin dashboard

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Usuario, Conversacion, Mensaje, Estado
 
-admin.site.register(Usuario)
 admin.site.register(Conversacion)
 admin.site.register(Mensaje)
 admin.site.register(Estado)
+
+@admin.register(Usuario)     #Si registro así una entidad en el admin, puedo personalizar las vistas en el panel del administrador.
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'email', 'fecha_registro')
+    search_fields = ('nombre', 'email')
+    list_filter = ('fecha_registro',)
+    
+
